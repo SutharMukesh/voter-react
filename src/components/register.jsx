1bc7ba4d0b29a287b5abda4bcb39373110f39ab9
Mukesh Suthar <mukeshsuthar336@gmail.com>
2020-01-28 22:42:50 +0530

    cleanup unnecessary code and add register comp

---
+++ src/components/register.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Register extends Component {
+    render() {
+        return (
+            <div className="container m-5">
+                <h3 className="mb-5">Voter Registration Form</h3>
+                <button type="button" className="btn btn-primary">test</button>
+            </div>
+        )
+    }
+}
