0dc20456550a4f5e2f4b366155caa81cf96020d4
Mukesh Suthar <mukeshsuthar336@gmail.com>
2020-02-08 17:43:09 +0530

    router with homepage nav tabs

--- src/App.js
+++ src/App.js
@@ -3,15 +3,46 @@ import Register from "./components/register";
 import SearchVoter from "./components/readvoter";
 import "./App.css";
 import { Provider } from "react-redux";
-import store from './store'
+import store from "./store";
+import {
+  NavLink,
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="d-flex">
-        <SearchVoter />
-      </div>
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <nav className="container nav mt-5 nav-tabs justify-content-center">
+          <NavLink
+            className="nav-item nav-link mr-2"
+            activeClassName="nav-item nav-link active"
+            to="/search"
+          >
+            Search
+          </NavLink>
+          <NavLink
+            className="nav-item nav-link ml-2"
+            activeClassName="nav-item nav-link active"
+            to="/register"
+          >
+            Register
+          </NavLink>
+        </nav>
+        <div className="d-flex">
+          <Switch>
+            <Route path="/search">
+              <SearchVoter />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+          </Switch>
+        </div>
+      </Provider>
+    </Router>
   );
 }
 
