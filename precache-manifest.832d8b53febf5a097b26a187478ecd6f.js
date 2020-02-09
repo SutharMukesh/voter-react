2c4181bcf21d7d9cdd06b29196f9216486a2f411
Mukesh Suthar <mukeshsuthar336@gmail.com>
2020-02-09 18:10:46 +0530

    Updates

---
+++ precache-manifest.832d8b53febf5a097b26a187478ecd6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf71e60aee54b1c64f494be3fbca8e7",
+    "url": "sutharmukesh.github.io/voter/index.html"
+  },
+  {
+    "revision": "d72ce7cce7c5025779e0",
+    "url": "sutharmukesh.github.io/voter/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "d72ce7cce7c5025779e0",
+    "url": "sutharmukesh.github.io/voter/static/js/2.549b799a.chunk.js"
+  },
+  {
+    "revision": "fe4be9f7a341140e33fc9d9f48d2392d",
+    "url": "sutharmukesh.github.io/voter/static/js/2.549b799a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e51e3ec810dd257c5b5",
+    "url": "sutharmukesh.github.io/voter/static/js/main.534815ef.chunk.js"
+  },
+  {
+    "revision": "71906c4a638826463920",
+    "url": "sutharmukesh.github.io/voter/static/js/runtime-main.ff2f98b5.js"
+  }
+]);
